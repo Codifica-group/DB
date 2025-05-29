f9774518f034caf195224f05e657ddcb6451c48c
Felippe-ASLopes <felippedias3@gmail.com>
2025-05-29 19:42:53 -0300

    :recycle: refact: categoria_produto

--- script.sql
+++ script.sql
@@ -26,13 +26,8 @@ CREATE TABLE cliente_pacote (
     id INT PRIMARY KEY AUTO_INCREMENT,
     cliente_id INT NOT NULL,
     pacote_id INT NOT NULL,
-<<<<<<< HEAD
-    data_inicio DATETIME,
-    data_expiracao DATETIME,
-=======
     data_inicio DATE,
     data_expiracao DATE,
->>>>>>> 82679b7b3fa0cfe91d93675facf33e402161d288
     FOREIGN KEY (cliente_id) REFERENCES cliente(id),
     FOREIGN KEY (pacote_id) REFERENCES pacote(id)
 );
@@ -69,7 +64,6 @@ CREATE TABLE agenda (
     pet_id INT NOT NULL,
     data_hora_inicio DATETIME NOT NULL,
     data_hora_fim DATETIME NOT NULL,
-    valor FLOAT NOT NULL,
     FOREIGN KEY (pet_id) REFERENCES pet(id)
 );
 
@@ -77,7 +71,7 @@ CREATE TABLE agenda_servico (
     id INT PRIMARY KEY AUTO_INCREMENT,
     agenda_id INT NOT NULL,
     servico_id INT NOT NULL,
-    concluido BOOLEAN NOT NULL DEFAULT FALSE,
+    valor FLOAT NOT NULL,
     FOREIGN KEY (agenda_id) REFERENCES agenda(id),
     FOREIGN KEY (servico_id) REFERENCES servico(id)
 );
@@ -116,21 +110,84 @@ VALUES ('Pequeno'), ('Médio'), ('Grande');
 
 -- Raças
 INSERT INTO raca (nome, porte_id)
-VALUES ('Poodle', 1), ('Golden Retriever', 3), ('Shih Tzu', 2), ('Bulldog', 1), ('Labrador', 3);
+VALUES 
+  -- Pequeno
+  ('Boston Terrier', 1),
+  ('Bulldog Francês', 1),
+  ('Chihuahua', 1),
+  ('Salsicha', 1),
+  ('Spitz Alemão', 1),
+  ('Maltês', 1),
+  ('Pequinês', 1),
+  ('Pinscher', 1),
+  ('Poodle', 1),
+  ('Shih Tzu', 1),
+  ('Yorkshire', 1),
+  ('Buldogue Inglês', 1),
+  ('Pug' , 1),
+  
+  -- Médio
+  ('Beagle', 2),
+  ('Cocker Spaniel', 2),
+  ('Fox Terrier', 2),
+  ('Schnauzer', 2),
+  ('Shar Pei', 2),
+  ('Bull Terrier', 2),
+  ('Whippet', 2),
+  ('Jack russell', 2),
+  ('Pit Bull', 2),
+  ('American Bully', 2),
+
+  -- Grande
+  ('Bernese', 3),
+  ('Boxer', 3),
+  ('Doberman', 3),
+  ('Dogue Alemão', 3),
+  ('Golden', 3),
+  ('Labrador', 3),
+  ('Pastor Alemão', 3),
+  ('Pastor Belga', 3),
+  ('Rottweiler', 3),
+  ('São Bernardo', 3),
+  ('Husky Siberiano', 3),
+  ('Weimaraner', 3);
 
 -- Categorias de despesas
 INSERT INTO categoria_produto (nome)
-VALUES ('Gasto fixo'), ('Manutenção'), ('Insumo'), ('Produto');
+VALUES ('Gasto fixo'), ('Manutenção'), ('Insumo');
 
+-- Produtos
 INSERT INTO produto (nome, categoria_produto_id)
-VALUES ('aluguel', 1);
+VALUES 
+  -- Gasto fixo
+  ('Aluguel', 1),
+  ('Conta de Luz', 1),
+  ('Conta de Água', 1),
+  ('Internet', 1),
+
+  -- Manutenção
+  ('Máquina de Tosa', 2),
+  ('Secador', 2),
+  ('Tesoura', 2),
+  ('Escova', 2),
+
+  -- Insumo
+  ('Algodão', 3),
+  ('Toalha', 3),
+  ('Papel Higiênico', 3),
+  ('Sacos de Lixo', 3),
+  ('Desinfetante', 3),
+  ('Shampoo', 3),
+  ('Condicionador', 3),
+  ('Sabonete', 3),
+  ('Perfume', 3);
 
 -- Serviços
 INSERT INTO servico (nome, valor_base)
 VALUES 
-    ('Banho', 40.00),
-    ('Tosa', 50.00),
-    ('Hidratação', 70.00);
+    ('Banho', 50.00),
+    ('Tosa', 60.00),
+    ('Hidratação', 20.00);
 
 -- Seleciona todos os pets com seus respectivos donos e raças
 SELECT 
@@ -158,7 +215,7 @@ SELECT
     cliente.nome AS cliente,
     pet.nome AS pet,
     servico.nome AS servico,
-    agenda.valor AS valor_final,
+    agenda_servico.valor AS valor_final,
     agenda.data_hora_inicio
 FROM agenda_servico
 JOIN agenda ON agenda_servico.agenda_id = agenda.id
@@ -204,7 +261,6 @@ SELECT
     produto.nome AS produto,
     categoria_produto.nome AS categoria
 FROM produto
-<<<<<<< HEAD
 JOIN categoria_produto ON produto.categoria_produto_id = categoria_produto.id;
 
 SELECT * FROM usuario;
@@ -214,8 +270,3 @@ SELECT * FROM raca;
 SELECT * FROM produto;
 
 SELECT * FROM categoria_produto;
-=======
-JOIN categoria_despesa ON produto.categoria_despesa_id = categoria_despesa.id;
-
--- DROP DATABASE eleve; --
->>>>>>> 82679b7b3fa0cfe91d93675facf33e402161d288
