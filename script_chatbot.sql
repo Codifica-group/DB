c14e83f32e8c6c7caef33a79b8c879990cb60528
Felippe-ASLopes <felippedias3@gmail.com>
2025-09-28 11:47:34 -0300

    :recycle: refact: relacionamento entre Chat e Cliente

--- script_chatbot.sql
+++ script_chatbot.sql
@@ -1,22 +1,17 @@
 CREATE DATABASE chatbot;
 USE chatbot;
 
-CREATE TABLE chat (
-    id INTEGER PRIMARY KEY,
-    passo_atual VARCHAR(50),
-    dados_contexto JSON,
-    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
 CREATE TABLE cliente (
     id INTEGER PRIMARY KEY,
     nome VARCHAR(50)
 );
 
-CREATE TABLE chat_cliente (
-    chat_id INTEGER PRIMARY KEY,
-    cliente_id INTEGER NOT NULL UNIQUE,
-	FOREIGN KEY(chat_id) REFERENCES chat(id),
+CREATE TABLE chat (
+    id INTEGER PRIMARY KEY,
+    passo_atual VARCHAR(50),
+    dados_contexto JSON,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    cliente_id INTEGER UNIQUE,
     FOREIGN KEY(cliente_id) REFERENCES cliente(id)
 );
 
@@ -25,7 +20,7 @@ SELECT
     chat.passo_atual,
     chat.dados_contexto,
     chat.data_atualizacao,
-    cliente.id AS cliente_id
+    cliente.id AS cliente_id,
+    cliente.nome AS cliente_nome
 FROM chat
-JOIN chat_cliente ON chat.id = chat_cliente.chat_id
-JOIN cliente ON chat_cliente.cliente_id = cliente.id;
+JOIN cliente ON chat.cliente_id = cliente.id;
